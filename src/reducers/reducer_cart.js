d956cabb6702b75ae40f74733ccb449e63e55bdf
rahul khanna <rahul.khanna@edfora.com>
2018-10-10 02:16:28 +0530

    ..

--- src/reducers/reducer_cart.js
+++ src/reducers/reducer_cart.js
@@ -4,11 +4,10 @@ import { SUBTRACT_FROM_CART, ADD_TO_CART } from "../actions";
 export default function(state = [], action) {
   switch (action.type) {
     case SUBTRACT_FROM_CART:
-      let ss = action.payload.all || state.find(item => item.id === action.payload.productId).count === 1 ? state.filter(item => item.id !== action.payload.productId) : state.map(item => {
+      return action.payload.all || state.find(item => item.id === action.payload.productId).count === 1 ? state.filter(item => item.id !== action.payload.productId) : state.map(item => {
         item.id === action.payload.productId ? item.count-- : ''
         return item
       });
-      return ss
     case ADD_TO_CART:
       return Boolean(state.find(item => item.id === action.payload.id)) ? state.reduce((acc, curr) => {
         curr.count = curr.id === action.payload.id ? ++curr.count : curr.count
